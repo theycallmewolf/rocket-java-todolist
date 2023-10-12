d288b1dfe00145028101c24acc2fd236e5a55aeb
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-12 21:30:25 +0100

    Adds `Project Lombok`

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+- [RocketSeat course details](https://efficient-sloth-d85.notion.site/Aqui-voc-encontra-todas-as-informa-es-986c7eb3fcb641ffb57b25ab025295ab)
+- [spring initializr](https://start.spring.io/)
+- [spring boot reference](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/)
+- [Project Lombok](https://projectlombok.org/)
--- src/main/java/com/theycallmewolf/todolist/users/UserModel.java
+++ src/main/java/com/theycallmewolf/todolist/users/UserModel.java
@@ -1,32 +1,14 @@
 package com.theycallmewolf.todolist.users;
 
+// import lombok.Getter;
+// import lombok.Setter;
+import lombok.Data;
+
+// @Getter
+// @Setter
+@Data
 public class UserModel {
    private String username;
    private String name;
    private String password;
-
-   // getters and setters for username, name, and password
-   public void setUsername(String username) {
-       this.username = username;
-   }
-
-   public String getUsername() {
-       return this.username;
-   }
-
-   public void setName(String name) {
-       this.name = name;
-   }
-
-   public String getName() {
-       return this.name;
-   }
-
-   public void setPassword(String password) {
-       this.password = password;
-   }
-
-   public String getPassword() {
-       return this.password;
-   }
 }

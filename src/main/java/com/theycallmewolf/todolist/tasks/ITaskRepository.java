48a23169dfb23bb1d6d41cc338fb376243b60d81
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-13 11:43:25 +0100

    Adds `/tasks/crete` route

---
+++ src/main/java/com/theycallmewolf/todolist/tasks/ITaskRepository.java
@@ -0,0 +1,9 @@
+package com.theycallmewolf.todolist.tasks;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    
+}
---
+++ src/main/java/com/theycallmewolf/todolist/tasks/TaskController.java
@@ -0,0 +1,22 @@
+package com.theycallmewolf.todolist.tasks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/create")
+    public TaskModel create(@RequestBody TaskModel taskModel) {
+        var task = this.taskRepository.save(taskModel);
+        return task;
+    }
+
+}

66dee2ac4e3447e61d5f2afea2da7870615e94dc
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-14 00:58:06 +0100

    Handle `message not readable` exception

--- src/main/java/com/theycallmewolf/todolist/tasks/TaskModel.java
+++ src/main/java/com/theycallmewolf/todolist/tasks/TaskModel.java
@@ -32,4 +32,12 @@ public class TaskModel {
     private LocalDateTime createdAt;
 
     private UUID userId;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50) {
+            throw new Exception("Title must be less than 50 characters");
+        }
+
+        this.title = title;
+    }
 }

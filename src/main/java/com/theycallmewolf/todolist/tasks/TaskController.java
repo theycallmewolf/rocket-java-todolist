44f4fad22943026387d7db142ea5d90125275dee
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-13 18:39:18 +0100

    Handle `create/task` date validations

--- src/main/java/com/theycallmewolf/todolist/tasks/TaskController.java
+++ src/main/java/com/theycallmewolf/todolist/tasks/TaskController.java
@@ -1,8 +1,11 @@
 package com.theycallmewolf.todolist.tasks;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,27 @@ public class TaskController {
     private ITaskRepository taskRepository;
     
     @PostMapping("/create")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
 
         var userId = request.getAttribute("userId");
         taskModel.setUserId((UUID) userId);
 
+        var currentDate = LocalDateTime.now();
+
+        // Check if start date is in the past
+        if(currentDate.isAfter(taskModel.getStartDate()) || currentDate.isAfter(taskModel.getEndDate())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Start and/or end date(s) must be in the future");
+        }
+
+        // Check if end date is before start date
+        if(taskModel.getEndDate().isBefore(taskModel.getStartDate())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("End date must be after start date");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
 }

4bb5851b78ed136c2bbfee21420e511d69053d1f
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-13 10:31:43 +0100

    Creates `Users` table

--- src/main/java/com/theycallmewolf/todolist/users/UserModel.java
+++ src/main/java/com/theycallmewolf/todolist/users/UserModel.java
@@ -1,5 +1,14 @@
 package com.theycallmewolf.todolist.users;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+// import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 // import lombok.Getter;
 // import lombok.Setter;
 import lombok.Data;
@@ -7,8 +16,18 @@ import lombok.Data;
 // @Getter
 // @Setter
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+   
+   @Id
+   @GeneratedValue(generator = "UUID")
+   private UUID id;
+
+   // @Column(name = "user") table column name = "user", variable name = "username"
    private String username;
    private String name;
    private String password;
+
+   @CreationTimestamp
+   private LocalDateTime createdAt;
 }

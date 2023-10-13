5231176b1b89c4c3d7229ff31e72880d425d8530
They Call Me Wolf <bruno.m.lobato@gmail.com>
2023-10-13 11:29:33 +0100

    Adds `TaskModel`

---
+++ src/main/java/com/theycallmewolf/todolist/tasks/TaskModel.java
@@ -0,0 +1,35 @@
+package com.theycallmewolf.todolist.tasks;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data // generates getters and setters
+@Entity(name = "tb_tasks") // maps this class to the `tb_tasks` table
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+     @Column(length = 10)
+    private String priority;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID userId;
+}
